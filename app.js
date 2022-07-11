198e883871178b8fc8c1c9be34e8a9e0bb586e5e
Ali <59686248+Alyster27@users.noreply.github.com>
2022-07-11 15:21:15 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+
+document.querySelector('.circle').addEventListener('click', () => {
+    document.querySelector('.vertical').classList.toggle('open');
+    document.querySelector('.line-wrapper').classList.toggle('open');
+    document.querySelector('.content').classList.toggle('open');
+});
